6d7643da87601b13242c19e716faff1e21b44b67
Trevor Schmidt <trevorjschmidt97@gmail.com>
2022-05-06 19:14:34 -0600

    Test

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Home from "./components/Home";
+import { Text } from "react-native";
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return <Text>Hello</Text>;
   }
 }
